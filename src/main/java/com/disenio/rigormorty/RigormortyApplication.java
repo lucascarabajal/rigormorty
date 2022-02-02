aa2118bb65714a4d168d9eeed1c048906eaceca6
Lucas Carabajal <lucascar007@gmail.com>
2022-02-02 09:44:57 -0300

    Primer commit

---
+++ src/main/java/com/disenio/rigormorty/RigormortyApplication.java
@@ -0,0 +1,13 @@
+package com.disenio.rigormorty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RigormortyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RigormortyApplication.class, args);
+	}
+
+}
