0375d2ba7f11810e7d12edb9ab669088b22622a1
Lucas Carabajal <lucascar007@gmail.com>
2022-02-21 10:13:24 -0300

    Agregando ZonaRepository

---
+++ src/main/java/com/disenio/rigormorty/Repository/ZonaRepository.java
@@ -0,0 +1,9 @@
+package com.disenio.rigormorty.Repository;
+
+import com.disenio.rigormorty.entity.Zona;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ZonaRepository extends JpaRepository<Zona,String> {
+}
