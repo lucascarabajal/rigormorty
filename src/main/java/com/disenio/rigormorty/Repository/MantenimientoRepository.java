c165e309f203e82fbf999b79d4f6664c78bb954d
Lucas Carabajal <lucascar007@gmail.com>
2022-02-21 10:01:04 -0300

    Agregando MantenimientoRepository

---
+++ src/main/java/com/disenio/rigormorty/Repository/MantenimientoRepository.java
@@ -0,0 +1,9 @@
+package com.disenio.rigormorty.Repository;
+
+import com.disenio.rigormorty.entity.Mantenimiento;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MantenimientoRepository extends JpaRepository<Mantenimiento,Long> {
+}
