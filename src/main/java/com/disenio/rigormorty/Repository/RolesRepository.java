3b38ba5e7307015c53cb7d6f5b46997caf67d897
Lucas Carabajal <lucascar007@gmail.com>
2022-02-21 10:08:28 -0300

    Agregando RolesRepository

---
+++ src/main/java/com/disenio/rigormorty/Repository/RolesRepository.java
@@ -0,0 +1,9 @@
+package com.disenio.rigormorty.Repository;
+
+import com.disenio.rigormorty.entity.Roles;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RolesRepository extends JpaRepository<Roles,Long> {
+}
