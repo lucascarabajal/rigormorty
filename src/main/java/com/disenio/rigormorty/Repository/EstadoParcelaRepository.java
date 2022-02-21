79091adbacc986bdca3595d641b03f5691aa6ced
Lucas Carabajal <lucascar007@gmail.com>
2022-02-21 09:45:11 -0300

    Agregando EstadoParcelaRepository

---
+++ src/main/java/com/disenio/rigormorty/Repository/EstadoParcelaRepository.java
@@ -0,0 +1,8 @@
+package com.disenio.rigormorty.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EstadoParcelaRepository extends JpaRepository {
+}
