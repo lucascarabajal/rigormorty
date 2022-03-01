7392d4d52c8b8048755110f46cd265f1e3a5f510
carlosKachuk <carloskachuk@gmail.com>
2022-03-01 16:51:52 -0300

    controller implemented

---
+++ src/main/java/com/disenio/rigormorty/controller/PeriodoController.java
@@ -0,0 +1,38 @@
+package com.disenio.rigormorty.controller;
+
+import com.disenio.rigormorty.entity.Periodo;
+import com.disenio.rigormorty.service.PeriodoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RequestMapping("/periodo")
+@RestController
+public class PeriodoController {
+    @Autowired
+    private PeriodoService periodoService;
+
+    @PostMapping
+    public ResponseEntity<Periodo> addPeriodo(@RequestBody Periodo periodo){
+        return periodoService.addPeriodo(periodo);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Periodo>> getPeriodos(){
+        return  periodoService.getPeriodo();
+    }
+
+//    @GetMapping
+//    public ResponseEntity<Parcela> getParcelaById(@PathVariable("id") Long id){
+//        return parcelaService.getParcelaById()
+//    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<Object> updatePeriodo(@PathVariable Long id, @RequestBody Periodo periodo) {
+        periodo.setId(id);
+        return ResponseEntity.ok().body(this.periodoService.updatePeriodo(periodo));
+    }
+
+}
---
+++ src/main/java/com/disenio/rigormorty/controller/RolesController.java
@@ -0,0 +1,34 @@
+package com.disenio.rigormorty.controller;
+
+import com.disenio.rigormorty.entity.Roles;
+import com.disenio.rigormorty.service.RolesService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RequestMapping("/roles")
+@RestController
+public class RolesController {
+
+    @Autowired
+    private RolesService rolesService;
+
+    @PostMapping
+    public ResponseEntity<Roles> addRol(@RequestBody Roles roles){
+        return rolesService.addRol(roles);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Roles>> getRoles(){
+        return  rolesService.getRol();
+    }
+
+
+    @PutMapping("{id}")
+    public ResponseEntity<Object> updateRoles(@PathVariable Long id, @RequestBody Roles roles) {
+        roles.setId(id);
+        return ResponseEntity.ok().body(this.rolesService.updateRol(roles));
+    }
+}
